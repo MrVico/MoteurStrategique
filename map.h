155d235b7f9a3baf6f3135d28e3d20c648eac419
tentrenche <tentrenche@gmail.com>
2019-01-16 11:39:16 +0100

    fix map

--- map.h
+++ map.h
@@ -6,24 +6,37 @@
 class map
 {
 public:
-    int w;
-    int h;
+    int w = 1080;
+    int h = 720;
 
-    char m[8][8];
+    int m[1080][720] = {{0}};
 
-    map() {
-        char t[8][8] = {
-            {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 1, 1, 1, 0}, {0, 0, 1, 0, 0, 0, 1, 0},
-            {0, 0, 1, 0, 0, 0, 1, 0}, {0, 0, 1, 1, 1, 1, 1, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}
-        };
-        w = h = 8;
-        for( int r = 0; r < h; r++ )
-            for( int s = 0; s < w; s++ )
-                m[s][r] = t[r][s];
+    void addObstacle(int x, int y) {
+        //en haut a gauche
+        for(int i = x; i < x + 32; ++i) {
+            for(int j = y; j < y + 32; ++j) {
+                m[i][j] = 1;
+            }
+        }
+    }
 
-        std::cout << "map ok" << std::endl;
+    void printMap() {
+        for(int i = 0; i < w; ++i) {
+            for(int j = 0; j < h; ++j) {
+                std::cout << m[i][j];
+            }
+
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    void setMap(int map[1080][720]) {
+        for(int i = 0; i < w; ++i) {
+            for(int j = 0; j < h; ++j) {
+                m[i][j] = map[i][j];
+            }
+        }
     }
 
     int operator() ( int x, int y ) { return m[x][y]; }
